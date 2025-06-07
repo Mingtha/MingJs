a7b36325e6ee2d2a4abd962df819b518b6d0507b
Mingda <mingtha@qq.com>
2025-06-07 17:48:11 +0800

    workflow

---
+++ rules/Update.py
@@ -0,0 +1,22 @@
+import yaml
+import requests
+import os
+
+green_url = (
+    "https://raw.githubusercontent.com/QCEnjoyLL/Mihomo-Scripts/506f3ade6e5260606daba06ad8e3dc1dd784ff6d/green.yaml"
+)
+response = requests.get(green_url)
+green_list: dict = yaml.safe_load(response.text).get("+rules", [])
+output_dir = os.path.dirname(__file__)
+with open(f"{output_dir}/GreenClash.yaml", "w", encoding="utf-8") as file:
+    yaml.dump(
+        data={"payload": green_list},
+        stream=file,
+        default_flow_style=False,
+        allow_unicode=True,
+        indent=2,
+    )
+
+with open(f"{output_dir}/GreenQX.list", "w", encoding="utf-8") as file:
+    file.write("\n".join(green_list).replace("DOMAIN-SUFFIX", "HOST-SUFFIX,"))
+print(f"green_list 包含: {len(green_list)} 条规则")
